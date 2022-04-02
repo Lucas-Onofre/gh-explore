ffe9c0cf6d75011e1e2b7981a762d6ecaaddee6e
Lucas-Onofre <lucas_onofre_zte@hotmail.com>
2022-04-02 17:43:37 -0300

    typescript components

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,16 +3,14 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
 
-//https://api.github.com/users/Lucas-Onofre/repos
-
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com/unform/unform'
+interface Repository{
+  name: string;
+  description: string;
+  html_url: string;
 }
 
 export function RepositoryList(){
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
       fetch('https://api.github.com/users/Lucas-Onofre/repos')
@@ -26,7 +24,7 @@ export function RepositoryList(){
 
       <ul>
         {repositories.map(repository => {
-          return <RepositoryItem key={repository.id} repository={repository} />
+          return <RepositoryItem key={repository.name} repository={repository} />
         })}
       </ul>
     </section>
